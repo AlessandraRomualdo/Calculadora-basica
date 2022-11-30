3c16228194065fae5382ccce956a1fffd98e03e7
Alessandra Romualdo <106454369+AlessandraRomualdo@users.noreply.github.com>
2022-11-30 17:26:21 -0300

    comentário nas funções

--- script/script.js
+++ script/script.js
@@ -1,17 +1,19 @@
 
-
+//Inserir um numero na tela.
 const insert=(num)=>{
     let valor = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = valor + num;
 };
-
+//Limpar os valores na tela.
 const limpar=()=>{
     document.getElementById('resultado').innerHTML = '';
 };
+//Remover os valores na tela um por vez.
 const apagar=()=>{
   let resultado = document.getElementById('resultado').innerHTML;
   document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
 };
+//Fazer calculos.
 const calcular=()=>{
     let resultado = document.getElementById('resultado').innerHTML;
     if(resultado){
